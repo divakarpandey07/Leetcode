4aa173db15576dadff3b13f21a4888aadbf2cc2a
Divakar Pandey <pandeydivakar07@gmail.com>
2026-09-19 09:45:32 +0530

    Time: 0 ms (100%), Space: 9 MB (80.88%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <climits>
+#include <cctype>
+
+class Solution {
+public:
+    int myAtoi(std::string s) {
+        int i = 0;
+        int n = s.length();
+        
+        while (i < n && s[i] == ' ') {
+            ++i;
+        }
+        
+     
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            ++i;
+        }
+        
+        
+        int result = 0;
+        while (i < n && std::isdigit(s[i])) {
+            int digit = s[i] - '0';
+            
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            
+            result = result * 10 + digit;
+            ++i;
+        }
+        
+        return result * sign;
+    }
+};
